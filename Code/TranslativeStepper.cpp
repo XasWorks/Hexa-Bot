37bc7925941a05c2590be16322e4ea26866601b6
Xasin <logger89@web.de>
2015-11-10 21:14:15 +0100

    Implementation of new stepper entity (0%)

---
+++ Code/TranslativeStepper.cpp
@@ -0,0 +1,14 @@
+/*
+ * TranslativeStepper.cpp
+ *
+ *  Created on: 10.11.2015
+ *      Author: xasin
+ */
+
+#include "TranslativeStepper.h"
+
+TranslativeStepper::TranslativeStepper() {
+	// TODO Auto-generated constructor stub
+
+}
+
---
+++ Code/TranslativeStepper.h
@@ -0,0 +1,16 @@
+/*
+ * TranslativeStepper.h
+ *
+ *  Created on: 10.11.2015
+ *      Author: xasin
+ */
+
+#ifndef CODE_TRANSLATIVESTEPPER_H_
+#define CODE_TRANSLATIVESTEPPER_H_
+
+class TranslativeStepper {
+public:
+	TranslativeStepper();
+};
+
+#endif /* CODE_TRANSLATIVESTEPPER_H_ */
