5ffc24d8e0911b025e1d1e1550a30e5e2d5fd3b6
Xasin <logger89@web.de>
2016-01-13 17:52:02 +0100

    First running implementation of new systems!

--- EclipseWS/Zephyr_Functional/main.cpp
+++ EclipseWS/Zephyr_Functional/main.cpp
@@ -3,17 +3,44 @@
 #include <util/delay.h>
 
 #include "Code/System/Robot.h"
+#include "Code/LineFollow/LF3Sens.h"
 
-Robot sys = Robot();
+Robot System = Robot();
+LF3Sens LFSensor = LF3Sens();
 
 ISR(TIMER1_COMPA_vect) {
-	sys.update();
+	System.update();
+	LFSensor.update();
 }
 
 
 int main() {
 
+	System.Motor.setAcceleration(500);
+	System.Motor.setSpeed(100);
+	System.Motor.setRotationSpeed(100);
+
 	while(true) {
+		if(System.Motor.atPosition())
+			System.Motor.moveTowards(50);
+
+
+		if(System.Motor.atRotation()) {
+			if(LFSensor.lineStatus == LF_OK) {
+				System.Motor.setRotationSpeed(80);
+				if(LFSensor.lineOffset > 0)
+					System.Motor.rotateBy(-1);
+				if(LFSensor.lineOffset < 0)
+					System.Motor.rotateBy(1);
+			}
+			else if(LFSensor.lineStatus == LF_LOST) {
+				System.Motor.setRotationSpeed(200);
+				if(LFSensor.lineOffset > 0)
+					System.Motor.rotateBy(-1);
+				if(LFSensor.lineOffset < 0)
+					System.Motor.rotateBy(1);
+			}
+		}
 
 	}
 
