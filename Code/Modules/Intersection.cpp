db29f4ffeb3631d554999991e74a161fff792f34
Xasin <logger89@web.de>
2016-02-22 16:51:13 +0100

    More light stupidifications :3

--- Code/Modules/Intersection.cpp
+++ Code/Modules/Intersection.cpp
@@ -19,27 +19,14 @@ namespace Module {
 		this->sys->Motor.setRotationSpeed(90);
 
 		// Checking if there is a line infront of us!
-		this->sys->Motor.moveTowards(100);
+		this->sys->Motor.moveTowards(FWD_CHECK_DIST);
 		this->sys->Motor.flush();
 		if(this->sens->lineStatus == LF_OK)
 			return;
 
 		//Returning to original position
-		this->sys->Motor.moveTowards(-100);
+		this->sys->Motor.moveTowards(INTSEC_DIST - FWD_CHECK_DIST);
 		this->sys->Motor.flush();
-
-		//Backing up ...
-		uint16_t passedSteps = 0;
-		while(this->sens->lineOffset != 0) {
-			if(passedSteps ++ > 100)
-				return;
-			this->sys->Motor.moveTowards(-1);
-			this->sys->Motor.flush();
-		}
-		this->sys->Motor.moveTowards(10);
-		this->sys->Motor.flush();
-
-		this->sys->Motor.moveTowards(INTSEC_DIST);
 
 		this->sys->Motor.rotateBy( (this->sens->lineOffset > 0) ? -90 : 90);
 	}
