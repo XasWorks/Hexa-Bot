43f424144db01a4980afe6fbad05b2f332bb70c9
Xasin <logger89@web.de>
2016-02-22 15:49:36 +0100

    First implementation of the advanced intsec code.

--- Code/Modules/Intersection.cpp
+++ Code/Modules/Intersection.cpp
@@ -18,10 +18,36 @@ namespace Module {
 		this->sys->Motor.setSpeed(50);
 		this->sys->Motor.setRotationSpeed(90);
 
-		this->sys->Motor.moveTowards(INTSEC_DIST);
-		this->sys->Motor.rotateBy((sens->lineOffset > 0) ? 90 : -90);
-
+		// Checking if there is a line infront of us!
+		this->sys->Motor.moveTowards(50);
 		this->sys->Motor.flush();
+		if(this->sens->lineStatus == LF_OK)
+			return;
+
+		//Returning to original position
+		this->sys->Motor.moveTowards(-50);
+		this->sys->Motor.flush();
+
+		//Backing up ...
+		while(this->sens->lineOffset != 0) {
+			this->sys->Motor.moveTowards(-1);
+			this->sys->Motor.flush();
+		}
+		this->sys->Motor.moveTowards(-10);
+		this->sys->Motor.flush();
+
+		//Closing in again
+		this->sys->Motor.setSpeed(10);
+		while(this->sens->lineOffset == 0) {
+			this->sys->Motor.moveTowards(0.1);
+			this->sys->Motor.flush();
+		}
+
+		this->sys->Motor.setSpeed(50);
+		this->sys->Motor.moveTowards(INTSEC_DIST);
+
+		this->sys->Motor.rotateBy( (this->sens->lineOffset > 0) ? -90 : 90);
+
 	}
 
 } /* namespace Module */
