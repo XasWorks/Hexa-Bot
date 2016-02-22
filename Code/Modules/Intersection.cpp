037f85b0ea044368128ce65633381c911c45aca3
Xasin <logger89@web.de>
2016-02-22 16:04:54 +0100

    Added break-out code in case of false activation

--- Code/Modules/Intersection.cpp
+++ Code/Modules/Intersection.cpp
@@ -15,21 +15,24 @@ namespace Module {
 	}
 
 	void Intersection::execute() {
-		this->sys->Motor.setSpeed(50);
+		this->sys->Motor.setSpeed(100);
 		this->sys->Motor.setRotationSpeed(90);
 
 		// Checking if there is a line infront of us!
-		this->sys->Motor.moveTowards(50);
+		this->sys->Motor.moveTowards(30);
 		this->sys->Motor.flush();
 		if(this->sens->lineStatus == LF_OK)
 			return;
 
 		//Returning to original position
-		this->sys->Motor.moveTowards(-50);
+		this->sys->Motor.moveTowards(-30);
 		this->sys->Motor.flush();
 
 		//Backing up ...
+		uint16_t passedSteps = 0;
 		while(this->sens->lineOffset != 0) {
+			if(passedSteps ++ > 30)
+				return;
 			this->sys->Motor.moveTowards(-1);
 			this->sys->Motor.flush();
 		}
@@ -38,14 +41,13 @@ namespace Module {
 
 		//Closing in again
 		while(this->sens->lineOffset == 0) {
-			this->sys->Motor.moveTowards(0.3);
+			this->sys->Motor.moveTowards(0.5);
 			this->sys->Motor.flush();
 		}
 
 		this->sys->Motor.moveTowards(INTSEC_DIST);
 
 		this->sys->Motor.rotateBy( (this->sens->lineOffset > 0) ? -90 : 90);
-
 	}
 
 } /* namespace Module */
