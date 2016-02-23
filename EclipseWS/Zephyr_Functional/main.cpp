8e69f0f6bf1d316aa8e13aa928a2339a0a1c84ee
Xasin <logger89@web.de>
2016-02-24 00:03:34 +0100

    Yay! :3

--- EclipseWS/Zephyr_Functional/main.cpp
+++ EclipseWS/Zephyr_Functional/main.cpp
@@ -45,7 +45,7 @@ void setTask() {
 			}
 
 			// Else, switch to Intersection mode
-			else if ((LFSensor.lineStatus == LF_INTSEC) /*|| ((PINC & ( 1<< 4)) != 0)*/) {
+			else if ((LFSensor.lineStatus == LF_INTSEC) || ((PINC & ( 1<< 4)) != 0)) {
 				currentTask = TASK_INTSEC;
 				cModule = &INTSECSys;
 			}
