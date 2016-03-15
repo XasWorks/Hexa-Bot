5b0e2e4049be06f7b0f3f5f474ce56f85a71bc4c
Xasin <logger89@web.de>
2016-03-15 17:18:38 +0100

    First usage of the Built-In PWM generator

--- EclipseWS/SensrChip/main.cpp
+++ EclipseWS/SensrChip/main.cpp
@@ -5,6 +5,35 @@
  *      Author: xasin
  */
 
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
+#define SERV_MIN 50
+#define SERV_MAX 150
 
+void setServo(uint8_t p) {
+	OCR2A = SERV_MIN + (p * (SERV_MAX - SERV_MIN) / 255);
+}
 
+int main() {
+
+	DDRD |= (1 << 7);
+
+	// Initialisation of PWM
+	DDRB |= (1 << PB3);
+
+	TCCR2A = (1 << COM2A1 | 1 << WGM21 | 1 << WGM20);
+	TCCR2B = (1 << CS22 | 1 << CS21);
+
+	OCR2A = 63;
+
+	uint8_t c = 0;
+	while(true) {
+		setServo(c++);
+		PORTD ^= (1<< 7);
+		_delay_ms(50);
+	}
+
+	return 0;
+}
